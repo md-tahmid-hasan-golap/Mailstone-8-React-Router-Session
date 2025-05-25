6db4280650029354f46de645948201222426b19c
md-tahmid-hasan-golap <golapraj47@gmail.com>
2025-05-25 23:40:12 +0600

    add router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router";
+const router = createBrowserRouter([
+  {
+    path: "/ph",
+    Component: App,
+  },
+  {
+    path: "/about",
+    element: <p>this is an about page</p>,
+  },
+]);
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router}></RouterProvider>
+  </StrictMode>
+);
